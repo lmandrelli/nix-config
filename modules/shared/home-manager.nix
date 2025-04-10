d4dbb2b8990892a7df9ee2fdb93cda17b038052e
Luca Mandrelli <luca.mandrelli@icloud.com>
2025-04-10 21:09:31 +0200

    git config to remove previous secrets config

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -194,19 +194,6 @@ let name = "Luca Mandrelli";
         "/Users/${user}/.ssh/config_external"
       )
     ];
-    matchBlocks = {
-      "github.com" = {
-        identitiesOnly = true;
-        identityFile = [
-          (lib.mkIf pkgs.stdenv.hostPlatform.isLinux
-            "/home/${user}/.ssh/id_github"
-          )
-          (lib.mkIf pkgs.stdenv.hostPlatform.isDarwin
-            "/Users/${user}/.ssh/id_github"
-          )
-        ];
-      };
-    };
   };
 
   tmux = {
