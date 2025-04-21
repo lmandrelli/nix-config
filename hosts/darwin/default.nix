d64aa038b66b0acdafab48926fa798ff10ff9a6a
Luca Mandrelli <luca.mandrelli@icloud.com>
2025-04-21 14:33:37 +0200

    ajoute des ajustements au volume et au feedback sonore

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -54,8 +54,8 @@ let user = "lmandrelli"; in
         InitialKeyRepeat = 15;
 
         "com.apple.mouse.tapBehavior" = 1;
-        "com.apple.sound.beep.volume" = 0.5;
-        "com.apple.sound.beep.feedback" = 1;
+        "com.apple.sound.beep.volume" = 1.0;
+        "com.apple.sound.beep.feedback" = true;
       };
 
       dock = {
@@ -74,6 +74,12 @@ let user = "lmandrelli"; in
       trackpad = {
         Clicking = true;
         TrackpadThreeFingerDrag = false;
+      };
+    };
+
+    activationScripts = {
+      suppressStartupSound = {
+        text = "nvram StartupMute=%01";
       };
     };
   };
